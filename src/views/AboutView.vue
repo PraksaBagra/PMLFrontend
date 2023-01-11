72659b42eda22c499b1400dfc82f5ef04546925f
Stefan Stankic <turtleskrofa@gmail>
2023-01-11 09:26:41 +0100

    new vue app version

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page KAKO NE RAZUMES</h1>
+    <p>Promeni Usera</p>
   </div>
 </template>
---
+++ src/views/UserPage.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+data: {
+
+},
+methods: {
+ LoadUserMovies(userId){
+    // get user movies from database
+    let movies = [];
+    return movies;
+ }
+},
+mounted: {
+LoadUserMovies();
+},
+created: {
+
+}
+}
+</script>
+
+<style>
+
+</style>
